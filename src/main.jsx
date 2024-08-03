76fead86b587a0d11e4531a2b2d3c3fa0d5aaad0
AlexandruARV <alex.alex12345100@yahoo.com>
2024-08-03 20:59:04 +0300

    v0.17

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { StyleSheetManager } from "styled-components";
 import isPropValid from "@emotion/is-prop-valid";
+import { DarkModeProvider } from "./context/DarkModeContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
-      <App />
+      <DarkModeProvider>
+        <App />
+      </DarkModeProvider>
     </StyleSheetManager>
   </React.StrictMode>
 );
